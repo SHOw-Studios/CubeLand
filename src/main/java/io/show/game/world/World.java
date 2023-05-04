b23472acab5eb50a362551947339ca652e70c09a
IliHanSoLow <ilian-ode@gmx.de>
2023-05-04 10:04:04 +0200

    World class introduced

---
+++ src/main/java/io/show/game/world/World.java
@@ -0,0 +1,8 @@
+package io.show.game.world;
+
+public class World {
+    public static final int MAP_MEDIUM_WIDTH = 6400;
+    public static final int MAP_MEDIUM_HIGHT = 2000;
+    public static final int MAP_LARGE_WIDTH = 8400;
+    public static final int MAP_LARGE_HIGHT = 2400;
+}
