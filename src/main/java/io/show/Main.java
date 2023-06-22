1085228d959591997a7be95fd3b27d6cb7c7fcc7
IliHanSoLow <ilian-ode@gmx.de>
2023-06-22 10:05:43 +0200

    main changed

--- src/main/java/io/show/Main.java
+++ src/main/java/io/show/Main.java
@@ -1,7 +1,9 @@
 package io.show;
 
+import io.show.game.GameLoop;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new GameLoop().init();
     }
 }
